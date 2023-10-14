5324a1bd861326daf8674ccc532e68dc0a672862
CristianJhair <cristianjhair2000@gmail.com>
2023-10-13 23:20:44 -0500

    maestro-detalle

---
+++ MaestroDetalle/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using MaestroDetalle.Models;
+using MaestroDetalle.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MaestroDetalle.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DbcompraContext _context;
+
+        public HomeController(DbcompraContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromBody] CompraVM oCompraVM)
+        {
+            //Objeto de tipo Compra-oCompra
+            Compra oCompra = oCompraVM.oCompra;
+
+            //Objeto de tipo Compra-oDetalleCompra
+            oCompra.DetalleCompras = oCompraVM.oDetalleCompra;
+
+            //Guardando objeto en tabla Compras
+            _context.Compras.Add(oCompra);
+            _context.SaveChanges();
+
+            return Json(new {respuesta=true});
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MaestroDetalle/Models/DbcompraContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaestroDetalle.Models;
+
+public partial class DbcompraContext : DbContext
+{
+    public DbcompraContext()
+    {
+    }
+
+    public DbcompraContext(DbContextOptions<DbcompraContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Compra> Compras { get; set; }
+
+    public virtual DbSet<DetalleCompra> DetalleCompras { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-2JLPE8T\\SQLEXPRESS; Database=DBCOMPRA; Trusted_Connection=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Compra>(entity =>
+        {
+            entity.HasKey(e => e.IdCompra).HasName("PK__COMPRA__0A5CDB5C9C42162C");
+
+            entity.ToTable("COMPRA");
+
+            entity.Property(e => e.NumeroDocumento)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.RazonSocial)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<DetalleCompra>(entity =>
+        {
+            entity.HasKey(e => e.IdDetalleCompra).HasName("PK__DETALLE___E046CCBB247F2E81");
+
+            entity.ToTable("DETALLE_COMPRA");
+
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.Producto)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Total).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.IdCompraNavigation).WithMany(p => p.DetalleCompras)
+                .HasForeignKey(d => d.IdCompra)
+                .HasConstraintName("FK_IdVenta");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
